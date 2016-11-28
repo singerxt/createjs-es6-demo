0aa5e99459a7bbf7f0a4f985a1c8d55faef797c2
singerxt <mat.spiewak@gmail.com>
2016-11-28 13:50:43 +0100

    Updated linting

--- src/modules/clouds/index.js
+++ src/modules/clouds/index.js
@@ -3,8 +3,8 @@
 import { Bitmap } from 'createjs-browserify'
 const clouds = new Bitmap('./assets/clouds.png')
 
-clouds.scaleX = 1;
-clouds.scaleY = 0.8;
+clouds.scaleX = 1
+clouds.scaleY = 0.8
 
 clouds.addEventListener('tick', () => {
   clouds.x -= 0.1
@@ -14,4 +14,4 @@ clouds.addEventListener('tick', () => {
   }
 })
 
-export default clouds;
+export default clouds
